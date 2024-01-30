ded3516543ac89251cfbe27493df73ab5ef6c59a
rabi27 <rabisiddique27@gmail.com>
2024-01-30 22:11:08 +0500

    decorater for validating key parameters

--- Cache.ts
+++ Cache.ts
@@ -2,47 +2,56 @@ export class Cache {
   private cache = {};
   constructor() {}
 
+  @validateKey
   public insert(key: string, value: unknown): void | never {
-    if (key === '') {
-      throw new Error('Invalid key. Key can not be an empty string.');
-    }
     this.cache[key] = value;
     console.log('Insertion Successful in the cache');
   }
 
+  @validateKey
   public get(key: string): void | never {
-    if (key === '') {
-      throw new Error('Invalid key. Key can not be an empty string.');
-    }
-    if (!this.cache.hasOwnProperty(key)) {
-      throw new Error('Invalid key. Key is not present in the cache');
-    }
-
     console.log('Key Retrieval Successful', this.cache[key]);
     return this.cache[key];
   }
 
+  @validateKey
   public delete(key: string): void | never {
-    if (key === '') {
-      throw new Error('Invalid key. Key can not be an empty string.');
-    }
-    if (!this.cache.hasOwnProperty(key)) {
-      throw new Error('Invalid key. Key is not present in the cache');
-    }
-
     console.log('Key Deletion Successful');
     delete this.cache[key];
   }
 
+  @validateKey
   public update(key: string, newValue: unknown): void | never {
-    if (key === '') {
-      throw new Error('Invalid key. Key can not be an empty string.');
-    }
-    if (!this.cache.hasOwnProperty(key)) {
-      throw new Error('Invalid key. Key is not present in the cache');
-    }
-
     console.log('Key Update Successful');
     this.cache[key] = newValue;
   }
+}
+
+function validateKey(target: any, key: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+
+  descriptor.value = function (...args: any[]) {
+    const [keyArgument, ...restArgs] = args;
+
+    if (keyArgument === '') {
+      throw new Error('Invalid key. Key cannot be an empty string.');
+    }
+
+    if (['get', 'delete', 'update'].includes(key)) {
+      if (!this.cache.hasOwnProperty(keyArgument)) {
+        throw new Error('Invalid key. Key is not present in the cache');
+      }
+    }
+
+    const result = originalMethod.apply(this, args);
+
+    console.log(
+      `Method ${key} called with arguments: ${JSON.stringify(
+        args
+      )}. Result: ${result}`
+    );
+    return result;
+  };
+
+  return descriptor;
 }
---
+++ Main.ts
@@ -0,0 +1,5 @@
+import { Cache } from './Cache';
+
+let cache = new Cache();
+cache.insert('Rabi', 'Siddique');
+cache.get('Rabi');
