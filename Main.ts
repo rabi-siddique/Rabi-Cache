6d1646e3477fd7350ed4240422d0b90f77437ca0
rabi27 <rabisiddique27@gmail.com>
2024-01-31 11:31:24 +0500

    adding ansi codes for colors and try catch block inside the getCommandLineInput function

--- Main.ts
+++ Main.ts
@@ -9,15 +9,26 @@ const cli = createInterface({
   output: process.stdout,
 });
 
+console.log("\x1b[32mYou're in Rabi Cache Terminal\x1b[0m");
 function getCommandLineInput() {
-  cli.question('Rabo: >>$ ', (userInput) => {
-    if (userInput.toLowerCase() === 'exit') {
-      cli.close();
-    } else {
-      executor.executeCommand(userInput);
+  cli.question('\x1b[32mRabi:>>$ \x1b[0m', (userInput) => {
+    try {
+      if (userInput.toLowerCase() === 'exit') {
+        console.log('\x1b[33mExiting...\x1b[0m');
+        cli.close();
+      } else {
+        executor.executeCommand(userInput);
+        getCommandLineInput();
+      }
+    } catch (error) {
       getCommandLineInput();
+      console.error(error.message);
     }
   });
 }
 
 getCommandLineInput();
+
+// Accepting connections
+// Running prev command with arrow keys
+// Timer to expire keys
